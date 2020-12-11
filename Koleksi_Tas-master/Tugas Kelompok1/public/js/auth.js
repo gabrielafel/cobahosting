97f805e637efc1868904854335bd8b5d2bb5508a
gabrielafel <70675316+gabrielafel@users.noreply.github.com>
2020-12-12 00:41:32 +0700

    push

--- Koleksi_Tas-master/Tugas Kelompok1/public/js/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    ensureAuthenticated: function (req, res, next) {
-      if (req.isAuthenticated()) {
-        return next();
-      }
-      res.render('pages/login', {layout: false});
-    },
-};
